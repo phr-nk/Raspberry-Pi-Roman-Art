c38e67e8b5fc8f5100d0dbe4146a26f349e1b98c
Phrank Lenoci <fcl1118@live.com>
2023-07-18 20:31:27 -0500

    inital commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Bust3D from "./Bust3D/Bust3D";
+
+function App() {
+  return (
+    <div className="App">
+      <Bust3D object="marcus_aurelius.glb" shaders={true} animation="follow" />
+    </div>
+  );
+}
+
+export default App;
